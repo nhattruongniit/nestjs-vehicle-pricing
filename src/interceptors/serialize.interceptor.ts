2e55c14cb7b46e177defc67233400d09a16c0386
Tony Nguyen <nhattruong1811@gmail.com>
2023-10-24 22:55:19 +0700

    doing 70

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,17 +1,31 @@
-import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-import { UserDto } from 'src/users/dtos/user.dto';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(
     _: ExecutionContext,
     handle: CallHandler,
   ): Observable<any> | Promise<Observable<any>> {
     return handle.handle().pipe(
       map((data: any) => {
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,7 @@ import {
   Param,
   Query,
   NotFoundException,
-  UseInterceptors,
+  // UseInterceptors,
 } from '@nestjs/common';
 
 import { UsersService } from './users.service';
@@ -16,11 +16,16 @@ import { UsersService } from './users.service';
 // dto
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 
 // interceptor
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import {
+  Serialize,
+  // SerializeInterceptor,
+} from '../interceptors/serialize.interceptor';
 
 @Controller('auth')
+@Serialize(UserDto) // want to apply this to all routes
 export class UsersController {
   constructor(private usersSerivce: UsersService) {}
 
@@ -29,11 +34,11 @@ export class UsersController {
     this.usersSerivce.create(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  // @Serialize(UserDto) // just apply this to this route
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersSerivce.findOne(parseInt(id));
-
     if (!user) {
       throw new NotFoundException('user not found');
     }
