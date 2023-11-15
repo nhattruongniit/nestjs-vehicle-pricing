9408047c9111b79826be9bf9e2ba7be2944e1486
truong.nn <truong.nn@amanotes.com>
2023-11-15 09:02:01 +0700

    nest js

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (_: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    request.session.userId;
-    return 'hi there';
+    return request.currentUser;
   },
 );
