9db74f785a65cbf61ea26ae80a1d7dfa877628e8
truong.nn <truong.nn@amanotes.com>
2023-11-23 11:32:42 +0700

    done unit test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,6 +5,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './users.entity';
+import exp from 'constants';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -21,49 +22,56 @@ describe('UsersController', () => {
         } as User);
       },
       find: (email) => {
-        return Promise.resolve([{
-          email,
-          password: 'asdf',
-        } as User])
+        return Promise.resolve([
+          {
+            email,
+            password: 'asdf',
+          } as User,
+        ]);
       },
       remove: (id) => {
         return Promise.resolve({
           id,
           email: 'asdf@asdf.com',
           password: 'asdf',
-        } as User)
+        } as User);
       },
       update: (id, user) => {
         return Promise.resolve({
           id,
           ...user,
-        } as User)
+        } as User);
       },
     };
     fakeAuthService = {
       signup: (email, password) => {
         return Promise.resolve({
+          id: 1,
           email,
           password,
-        } as User)
+        } as User);
       },
       signin: (email, password) => {
         return Promise.resolve({
+          id: 1,
           email,
           password,
-        } as User)
+        } as User);
       },
     };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [{
-        provide: UsersService,
-        useValue: fakeUserService,
-      }, {
-        provide: AuthService,
-        useValue: fakeAuthService,  
-      }]
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -76,5 +84,41 @@ describe('UsersController', () => {
   it('findUser throws an error if user with given id is not found', async () => {
     fakeUserService.findOne = () => null;
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
-  })
+    // fakeUserService.findOne = () => null;
+    // try {
+    //   await controller.findUser('1');
+    // } catch (e) {
+    //   done();
+    // }
+  });
+
+  it('findUser return a user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findAllUser return a list of users with the given email', async () => {
+    // given
+    const users = await controller.findAlluser('asdf@asdf.com');
+
+    // then
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('signin updates sesion object and return user', async () => {
+    // given
+    const session = { userId: -10 };
+    // when
+    const user = await controller.signin(
+      {
+        email: 'asdf@asdf.com',
+        password: 'asdf',
+      },
+      session,
+    );
+    // then
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  });
 });
