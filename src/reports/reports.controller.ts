2acd650b74fda1267f5a583a22d715c94b986978
truong.nn <truong.nn@amanotes.com>
2023-12-28 08:57:42 +0700

    add approve report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
-import { CreateReportDto } from './dtos/create-report.dto';
+import {
+  Controller,
+  Patch,
+  Param,
+  Post,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+
+// dto
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,7 +23,13 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
+  }
+
+  @Patch(':id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApprove(id, body.approved);
   }
 }
